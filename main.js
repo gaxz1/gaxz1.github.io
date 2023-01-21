2d8ca9a550771d999f1b5bc2e42edbdd9049a383
gaxz <123206273+gaxz1@users.noreply.github.com>
2023-01-22 01:35:12 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,4 @@
 let x = document.getElementById("dx");
 x.onclick = function(){
-  wc.alert('你好');
-};
-   	  wc.alertDialog({
-   	    title: '提示',
-   	    content: '欢迎体验WebCat自研wc.js框架，感谢您的使用！',
-   	    ok: '确定',
-   	    okFun: function() {
-   	      wc.alert('点击了确定');
-   	    }
-   	  });
+     alert('你好');
+}
