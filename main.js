c277544398bcbe9aa11eff49aa0240a69ed21d88
gaxz <123206273+gaxz1@users.noreply.github.com>
2023-01-23 08:08:40 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,25 @@
-let x = document.getElementById("dx");
+let x = document.getElementById("x");
 x.onclick = function(){
-  alert('你好');
+  let  img = document.querySelector('#img');
+  setTimeout(function(){
+   if (img.style.display == 'block') {
+      img.style.display = 'none';
+      x.innerText = '显示';
+   } else {
+     img.style.display = 'block';
+     x.innerText = '隐藏';
+   }
+  },50);
+}
+let bq = document.querySelector('#qb');
+img.onclick = function(){
+  setTimeout(function() {
+    bq.style.display = 'block';
+  }, 200);
+}
+let b = document.querySelector('#an');
+b.onclick = function(){
+  setTimeout(function(){
+    bq.style.display = 'none';
+  },200);
 }
